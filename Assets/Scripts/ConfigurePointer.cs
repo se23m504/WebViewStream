a3e4979e29446dcebda50f55e3ca19a73c1efe35
Santiago Lo Coco <se23m504@technikum-wien.at>
2024-10-27 08:59:01 +0100

    Remove comments

--- Assets/Scripts/ConfigurePointer.cs
+++ Assets/Scripts/ConfigurePointer.cs
@@ -7,17 +7,6 @@ using Microsoft.MixedReality.Toolkit.Input;
 
 public class ConfigurePointer : MonoBehaviour
 {
-    void Start()
-    {
-        // PointerUtils.SetMotionControllerRayPointerBehavior(PointerBehavior.AlwaysOff);
-        // PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOff);
-        // PointerUtils.SetHandRayPointerBehavior(PointerBehavior.AlwaysOff); --> This is the one we want to disable/enable
-    }
-
-    // make a toggle method for the hand ray pointer. use a bool to keep track of the state
-    // if the bool is true, disable the hand ray pointer
-    // if the bool is false, enable the hand ray pointer
-    // by default it is true.
     private bool handRayPointerEnabled = true;
 
     public void ToggleHandRayPointer()
