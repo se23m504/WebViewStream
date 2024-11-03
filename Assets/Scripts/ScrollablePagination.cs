916e250fa8fd88f65e569d9dda8869fce4a44054
Santiago Lo Coco <se23m504@technikum-wien.at>
2024-11-03 16:18:37 +0100

    Update scene and settings

---
+++ Assets/Scripts/ScrollablePagination.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using UnityEngine;
+
+public class ScrollablePagination : MonoBehaviour
+{
+    [SerializeField]
+    private ScrollingObjectCollection scrollView;
+
+    public ScrollingObjectCollection ScrollView
+    {
+        get
+        {
+            if (scrollView == null)
+            {
+                scrollView = GetComponent<ScrollingObjectCollection>();
+            }
+            return scrollView;
+        }
+        set
+        {
+            scrollView = value;
+        }
+    }
+
+    public void ScrollByTier(int amount)
+    {
+        Debug.Assert(ScrollView != null, "Scroll view needs to be defined before using pagination.");
+        scrollView.MoveByTiers(amount);
+    }
+}
