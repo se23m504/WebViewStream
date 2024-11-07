a89f01351f9ee4d79de4b750235f6c6d0ca2e790
Santiago Lo Coco <se23m504@technikum-wien.at>
2024-11-07 23:40:38 +0100

    Add center speech command

--- Assets/Scripts/ConfigureOrbital.cs
+++ Assets/Scripts/ConfigureOrbital.cs
@@ -55,4 +55,35 @@ public class ConfigureOrbital : MonoBehaviour
             canvas.transform.rotation = Quaternion.LookRotation(directionToCamera);
         }
     }
+
+    public void CenterCanvasesToUser()
+    {
+        List<GameObject> canvases = endpointLoader.GetInstantiatedItems();
+
+        Vector3 localOffset = new Vector3(-0.4f, 0.1f, 1f);
+
+        foreach (GameObject canvas in canvases)
+        {
+            Transform cameraTransform = Camera.main.transform;
+
+            canvas.transform.position = cameraTransform.position + cameraTransform.TransformDirection(localOffset);
+            // canvas.transform.rotation = Quaternion.LookRotation(canvas.transform.position - cameraTransform.position);
+            // canvas.transform.rotation = Quaternion.LookRotation(-cameraTransform.position);
+            // canvas.transform.rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+            canvas.transform.rotation = Quaternion.LookRotation(cameraTransform.forward, cameraTransform.up);
+
+            localOffset = new Vector3(localOffset.x + GetItemWidth(canvas), localOffset.y, localOffset.z);
+        }
+    }
+
+    private float GetItemWidth(GameObject item)
+    {
+        RectTransform rectTransform = item.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            return rectTransform.rect.width * rectTransform.lossyScale.x + 0.2f;
+        }
+
+        return 0.8f;
+    }
 }
